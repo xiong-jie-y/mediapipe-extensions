129dde91348e2e4408730a5240e2f046365881c0
xiong-jie-y <fwvillage@gmail.com>
2020-08-17 19:37:15 +0900

    Add handcrafted head yes-or-no gesture recognizer.

--- analyze_face_motion.py
+++ analyze_face_motion.py
@@ -32,6 +32,39 @@ def detection_analysis_dashboard():
 
         return positions
 
+    WINDOW_PERIOD_S_ = 1.0
+    def get_minmax_feature(face_landmark_lists):
+        recent_data = []
+        latest_timestamp = face_landmark_lists[-1].timestamp
+        for face_landmark_list in face_landmark_lists:
+            if (latest_timestamp - WINDOW_PERIOD_S_) < face_landmark_list.timestamp:
+                if len(face_landmark_list.data) == 0:
+                    continue
+                main_face = face_landmark_list.data[0]
+                recent_data.append(main_face)
+
+        if len(recent_data) == 0:
+            return None, None, None, None
+        xs = []
+        ys = []
+        for recent_datum in recent_data:
+            dir_vec = flu.simple_face_direction(recent_datum, only_direction=True)
+            xs.append(dir_vec[0])
+            ys.append(dir_vec[1])
+        return max(xs), min(xs), max(ys), min(ys)
+
+    def get_state(face_landmark_lists):
+        xmax, xmin, ymax, ymin = get_minmax_feature(face_landmark_lists)
+        if xmax is None:
+            return 0
+
+        if ymax > 0.4 and ymin <= 0.0:
+            return 1
+        elif xmax > 0.4 and xmin < -0.4:
+            return -1
+        else:
+            return 0
+
     # Choose result dir.
     result_folders_dir = st.text_input("Location of result folders.", "data/")
     result_folders = list(next(os.walk(result_folders_dir))[1])
@@ -46,7 +79,6 @@ def detection_analysis_dashboard():
 
     frame_index = st.slider("Index", 0, len(images))
 
-    # st.write(type(images[0].data))
     st.image(images[frame_index].data)
     st.write(multi_face_landmark_lists[frame_index].data)
     st.write(flu.simple_face_direction(multi_face_landmark_lists[frame_index].data[0], only_direction=True))
@@ -65,14 +97,20 @@ def detection_analysis_dashboard():
         multi_face_landmarks.timestamp
         for multi_face_landmarks in multi_face_landmark_lists
     ]
+    # aaa = range(0, )
+    predictions = [
+        get_state(multi_face_landmark_lists[:i])
+        for i in range(1, len(multi_face_landmark_lists))
+    ]
     import plotly.graph_objects as go
     fig = go.Figure(
         data=[
-            go.Scatter(x=timestamps, y=direction_ys),
-            go.Scatter(x=timestamps, y=direction_xs),
+            go.Scatter(x=timestamps, y=direction_ys, name="y", line = dict(width=4, dash='dot')),
+            go.Scatter(x=timestamps, y=direction_xs, name="x", line = dict(width=4, dash='dot')),
+            go.Scatter(x=timestamps, y=predictions, name="preds", line = dict(width=4, dash='dot')),
         ],
-        layout_title_text="A Figure Displayed with fig.show()"
     )
+    fig.update_layout(xaxis_title='timestamp')
     st.write(fig)
 
 
