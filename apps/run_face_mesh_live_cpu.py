429f2a6a9e7cb82bd85e89d01615636575a0ff44
xiong-jie-y <fwvillage@gmail.com>
2020-08-27 22:09:54 +0900

    Fix example.

--- apps/run_face_mesh_live_cpu.py
+++ apps/run_face_mesh_live_cpu.py
@@ -1,7 +1,7 @@
 import faulthandler
 faulthandler.enable()
 
-from pika.graph_runner_cpu import UpperBodyPoseTracker
+from pika.graph_runner_cpu import GraphRunnerCpu
 
-pose_tracker = UpperBodyPoseTracker()
+pose_tracker = GraphRunnerCpu("graphs/face_mesh_desktop_live_any_model_cpu.pbtxt")
 pose_tracker.run_live()
