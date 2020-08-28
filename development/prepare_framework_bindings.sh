8b0cdeb291ae7dbb54e42e520a9d6e7326a9b88a
xiong-jie-y <fwvillage@gmail.com>
2020-08-29 01:46:55 +0900

    add missing file.

--- development/prepare_framework_bindings.sh
+++ development/prepare_framework_bindings.sh
@@ -1,3 +1,6 @@
-bazel build --define=MEDIAPIPE_DISABLE_GPU=1 --action_env=PYTHON_BIN_PATH=/home/yusuke/miniconda3/envs/py37_ubuntu_vtuber/bin/python //pika:_framework_bindings.so
+set -e
+# bazel build -c opt --action_env tflite_with_xnnpack=true --define=MEDIAPIPE_DISABLE_GPU=1 --action_env=PYTHON_BIN_PATH=/home/yusuke/miniconda3/envs/py37_ubuntu_vtuber/bin/python //pika:_framework_bindings.so
+
+bazel build -c opt --define=MEDIAPIPE_DISABLE_GPU=1 --action_env=PYTHON_BIN_PATH=/home/yusuke/miniconda3/envs/py37_ubuntu_vtuber/bin/python //pika:_framework_bindings.so
 mv bazel-bin/pika/_framework_bindings.so mediapipe/python
-pip install -e .
+# pip install -e .
